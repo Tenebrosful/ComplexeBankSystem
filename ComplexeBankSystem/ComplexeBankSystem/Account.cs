6c0d56312ec7d51263106f3c5fd9d28364fcf6d6
Tenebrosful <hugobernard39@gmail.com>
2021-05-13 20:12:59 +0200

    Add initialAmount param in constructor

--- ComplexeBankSystem/ComplexeBankSystem/Account.cs
+++ ComplexeBankSystem/ComplexeBankSystem/Account.cs
@@ -1,12 +1,17 @@
-﻿namespace ComplexeBankSystem
+﻿using System;
+
+namespace ComplexeBankSystem
 {
     internal class Account : ASomethingAmount
     {
         public string Name { get; private set; }
 
-        public Account(string name)
+        public Account(string name, double initialAmount)
         {
+            if (initialAmount <= 0) throw new ArgumentException("Doit être strictement supérieur");
+
             Name = name;
+            Amount = initialAmount;
         }
     }
 }
