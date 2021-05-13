36c24bae69b6ef0d5d812185e0894053d0fd6da2
Tenebrosful <hugobernard39@gmail.com>
2021-05-14 01:06:07 +0200

    Create AccountFormula.cs

---
+++ ComplexeBankSystem/ComplexeBankSystem/Accounts/AccountFormula.cs
@@ -0,0 +1,23 @@
+﻿using ComplexeBankSystem.Accounts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexeBankSystem.Accounts
+{
+    class AccountFormula
+    {
+        #region Properties
+        public string Name { get; private set; }
+        public double MinimalInitialAmount { get; private set; }
+        public AccountType AccountType { get; private set; }
+        #endregion
+
+        public AccountFormula(string name, double minimalInitialAmount, AccountType accountType)
+        {
+            Name = name; MinimalInitialAmount = minimalInitialAmount; AccountType = accountType;
+        }
+    }
+}
