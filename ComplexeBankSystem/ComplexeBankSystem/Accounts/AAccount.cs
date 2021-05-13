eb8e5b234c82f153de31e89d082ec2f5839a648c
Tenebrosful <hugobernard39@gmail.com>
2021-05-13 22:00:07 +0200

    Rename folder

--- ComplexeBankSystem/ComplexeBankSystem/Accounts/AAccount.cs
+++ ComplexeBankSystem/ComplexeBankSystem/Accounts/AAccount.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using ComplexeBankSystem.Accounts.Enums;
 
-namespace ComplexeBankSystem
+namespace ComplexeBankSystem.Accounts
 {
     abstract class AAccount : ASomethingAmount
     {
         public string Name { get; private set; }
+        public AccountType AccountType { get; private set; }
 
-        public AAccount(string name, double initialAmount)
+        public AAccount(string name, double initialAmount, AccountType accountType)
         {
             if (initialAmount <= 0) throw new ArgumentException("Doit être strictement supérieur");
 
             Name = name;
             Amount = initialAmount;
+            AccountType = accountType;
         }
     }
 }
