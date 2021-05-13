95987aeb5bb7d5e4dfb8d6efaa7f8c8f412eedc4
Tenebrosful <hugobernard39@gmail.com>
2021-05-13 13:31:15 +0200

    Add properties and constructor

--- ComplexeBankSystem/ComplexeBankSystem/Bank.cs
+++ ComplexeBankSystem/ComplexeBankSystem/Bank.cs
@@ -5,5 +5,15 @@ namespace ComplexeBankSystem
 {
     class Bank
     {
+        private static readonly List<Bank> _banks = new List<Bank>();
+        public string Name { get; private set; }
+        public static IReadOnlyList<Bank> Banks { get; private set; } = _banks;
+        private Dictionary<Player, Account> _clients;
+        public IReadOnlyDictionary<Player, Account> Clients { get; private set; }
+
+        public Bank(string name)
+        {
+            Name = name; Clients = _clients;
+        }
     }
 }
