e61322743bd9c0c78b03f2545083e3b10ec7e348
Tenebrosful <hugobernard39@gmail.com>
2021-05-13 22:00:23 +0200

    Create StandardAccount.cs

---
+++ ComplexeBankSystem/ComplexeBankSystem/Accounts/StandardAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComplexeBankSystem.Accounts.Enums;
+
+namespace ComplexeBankSystem.Accounts
+{
+    class StandardAccount : AAccount
+    {
+        public StandardAccount(string name, double initialAmount) : base(name, initialAmount, AccountType.Standard)
+        {
+        }
+    }
+}
