b8314704251f8766dbfc4254d9f3c34468af0142
Tenebrosful <hugobernard39@gmail.com>
2021-05-13 20:50:43 +0200

    Clean code and real readonly for collections

--- ComplexeBankSystem/ComplexeBankSystem/Bank.cs
+++ ComplexeBankSystem/ComplexeBankSystem/Bank.cs
@@ -5,29 +5,36 @@ namespace ComplexeBankSystem
 {
     class Bank
     {
-        private static readonly List<Bank> _banks = new List<Bank>();
+        #region Properties
+        private static readonly List<Bank> _banks = new();
+        private readonly Dictionary<Player, Account> _clients;
+        private readonly List<Player> _owners;
+
+        public static IReadOnlyList<Bank> Banks => _banks;
+        public IReadOnlyDictionary<Player, Account> Clients => _clients;
+        public IReadOnlyList<Player> Owners => _owners;
+
         public string Name { get; private set; }
-        public static IReadOnlyList<Bank> Banks { get; private set; } = _banks;
-        private Dictionary<Player, Account> _clients;
-        public IReadOnlyDictionary<Player, Account> Clients { get; private set; }
-        /// <summary>
-        /// Sommme de l'argent présent sur tous les comptes de la banque
-        /// </summary>
-        public double TotalAccountMoney { get; private set; }
+        
         /// <summary>
         /// Argent réel que la Banque possède (L'argent maximum que les clients peuvent récupérer s'ils retirent tous leur argent en même temps)
         /// </summary>
         public double BankMoney { get; private set; }
 
-        public Bank(string name)
+        /// <summary>
+        /// Sommme de l'argent présent sur tous les comptes de la banque
+        /// </summary>
+        public double TotalAccountMoney { get; private set; }
+        #endregion
+
+        public Bank(string name, List<Player> owners)
         {
-            Name = name; Clients = _clients;
+            Name = name; _owners = owners; _clients = new Dictionary<Player, Account>();
         }
 
         public void NewClient(Player client)
         {
             if (client == null) throw new ArgumentNullException(nameof(client));
-            if (_clients == null) _clients = new Dictionary<Player, Account>();
 
             try
             {
